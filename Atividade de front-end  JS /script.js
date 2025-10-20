b47c126d42bcbab6288c13c98293e221bd842a7d
Gabriel Antônio <144959941+GabrielAnton10@users.noreply.github.com>
2025-10-20 10:12:04 -0300

    Fix typo in toLowerCase and update conditions
    codigo atualizado

--- Atividade de front-end  JS /script.js
+++ Atividade de front-end  JS /script.js
@@ -2,13 +2,18 @@ let input = document.getElementById("comida");
 let paragrafo = document.getElementById("mensagem");
 
 input.addEventListener("input",()=>{
-    let prato = input.value.trim().toLowercase();
+    let prato = input.value.trim().toLowerCase();
+
+    if (!prato) {
+        paragrafo.textContent = "";
+        return;
+    }
 
     if (prato === "pão com ovo"){
         paragrafo.textContent = "Café da manhã.";
-    }else if (prato === "feijão com arroz e carne."){
-         paragrafo.textContent = "Almoço";
-    }else{
+    } else if (prato === "feijão com arroz e carne"){
+        paragrafo.textContent = "Almoço";
+    } else {
         paragrafo.textContent = "Jantar";
     }
 
